4859efff01e73491d98b6772c27de7538779b22d
jakob-wirbel <jakob.wirbel@gmail.com>
2017-06-22 13:37:32 +0200

    cleaned a bit more (not finished yet).

--- code/single_model_optimization/OptAllPatients.R
+++ code/single_model_optimization/OptAllPatients.R
@@ -2,44 +2,16 @@
 # Marti Bernardo-Faura, September 2014
 
 # Minor changes to adjust the paths to the combiMS Github project
+# Removed processing and normalization part --> moved to normalization_pipeline.R
 # Jakob Wirbel, June 2017
 
 # ****************** Set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-
 source("./OptCombiMS.R")
-source("../data_processing_and_normalization/normaliseSimp.R")
-source("../data_processing_and_normalization/processCombiData.R")
 
+filenames=list.files("../../data/phosphos_processed/",pattern="*.csv",full.names=FALSE)
 
-#filenames=list.files(".",pattern="*.csv",full.names=FALSE)
-
-#filenames=list.files("/Users/marti/Documents/ebi/combiMS/data/midas_clean_merged/processedForCNO/",pattern="*.csv",full.names=FALSE)
-filenames=list.files("../../data/phosphos_merged/",pattern="*.csv",full.names=FALSE)
-# setwd("/Users/marti/Documents/R/combiMS/modelling/final")
-
-
-
-# ****************** process all files to remove controls and be consistent with PKN and midas-like formattted
-# do only once :)
-#   
-# for (i in 1:length(filenames)){
-#   processCombiData(filenames[i]) 
-# }
-
-# ****************** normalise all files
-
-
-for (i in 1:length(filenames)){
-  fileName=filenames[i]
-  #fileName=paste(head(strsplit(filenames[i],"\\_")[[1]],n=1),".csv",sep="") #take only filename
-  taula=read.csv(paste("../../data/phosphos_merged/",fileName,sep=""),header=TRUE,dec=".",check.names=FALSE, stringsAsFactors=FALSE)
-  taula=normaliseSimp(taula)
-  # setwd("/Users/marti/Documents/ebi/combiMS/data/phosphosMergedAbsMax/processed/normalized/")
-  write.table(taula,file=paste0('../../data/phosphos_normalised/', fileName),sep=",",row.names=FALSE,quote=F)
-}
-  
 # ****************** use data from all patients to optimize one model per patient
 networks=list()
 for (i in 1:length(filenames)){
@@ -47,21 +19,12 @@ for (i in 1:length(filenames)){
   networks[[i]]=OptCombiMS(filenames[i])
 }
 
-# for (i in 1:6){
-#   
-#   networks[[i]]=OptCombiMS(filenames[1])
-# }
-
-
-
-save(networks,file="networks.Rdata")
-#write.table(networks,file="/Users/marti/Documents/R/combiMS/modelling/networksIC5001.csv",sep=",",row.names=FALSE,quote=F)
+save(networks,file="../../files/modeling/local/networks.Rdata")
 
 #networks=read.csv("./networksIC5006.csv",header=TRUE,dec=".",check.names=TRUE, stringsAsFactors=FALSE)
 
+
 # let's create the annotation labels for a nice aheatmap
-
-
 # let's get from the optimization results only the average networks for each patient
 #averageNetworks=lapply(networks,function(x){x$averageNw}) #not good, creates a list of lists
 averageNetworks=do.call(rbind, lapply(networks,function(x){x$averageNw}))
