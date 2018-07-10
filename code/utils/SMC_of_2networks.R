b5ce6c571007a0851a73aa5c0379f8ec5910c63a
Mel-Ri <rinas@combine.rwth-aachen.de>
2018-07-10 23:58:41 +0200

    I would like to use the newly implemented function SMC_of_2networks.R instead of the newJaccard.R function to make clear that we use the SMC = simple matching coefficient as measure for similarity

---
+++ code/utils/SMC_of_2networks.R
@@ -0,0 +1,43 @@
+
+# SMC_of_2networks.R
+
+# Script to calculate the similarity of 2 Boolean networks represented as bStrings 
+# by using the SMC
+
+
+# SMC = simple matching coefficient
+# (e.g. see https://en.wikipedia.org/wiki/Simple_matching_coefficient)
+# 
+# is a measure to calculate similarity
+# 
+
+# by 
+# Melanie Rinas
+# December 2017
+
+
+
+
+
+
+SMC_of_2networks_MR <- function(bString_network1, bString_network2) {     
+   
+   sum(bString_network1 == bString_network2) / length(bString_network1)
+   
+}
+
+
+
+
+
+
+# 
+# # Testing SMC_fct:
+# 
+# n1=c(0,0,1,1)
+# n2=c(0,1,0,1)
+# 
+# number_of_matching_attributes__M00_plus_M11 = sum(n1 == n2)
+# number_of_attributes__M00_plus_M01_plus_M10_plus_M11 = length(n1)
+# 
+# SMC_fct(n1,n2)
