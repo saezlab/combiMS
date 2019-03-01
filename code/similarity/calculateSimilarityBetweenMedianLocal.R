fed5c85688d62ebba18712da66e342c7c58c966a
Mel-Ri <rinas@combine.rwth-aachen.de>
2019-03-01 16:48:24 +0100

    Resolve self created issue through mixing the file name ...Local.R and ...Model.R

--- code/similarity/calculateSimilarityBetweenMedianLocal.R
+++ code/similarity/calculateSimilarityBetweenMedianLocal.R
@@ -1,43 +1,42 @@
-
 #***********************************************************************
 # This scripts loads the results of loadAllModelsPlaneNW2.R,
 # which is the concatenation of all networks solutions within 5% reltol for 
 # the runs that did not crush out of 10 attempts per patient
-# 
-# by Marti Bernardo-Faura.
-# 
-# 
-# 
-# adapted by Melanie Rinas, 2019
 #***********************************************************************
 
 # Use relative paths instead of absolut paths for the files
 # in Rstudio
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-# setwd('~/Documents/combiMS/code/similarity/')
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd('~/Documents/combiMS/code/similarity/')
 
+library(pryr)
 
-source("../utils/SMC_of_2networks.R")    
+source("./SMC_of_2networks.R")
 
+argsJob= commandArgs(trailingOnly=TRUE)
 
+patients = read.csv('../../files/annot169pat_v2.csv')$ID
 
-files_Similarity_folder = "../../files/similarity"
+patientOneOriginal=toString(patients[as.numeric(argsJob[1])])
+patientTwoOriginal=toString(patients[as.numeric(argsJob[2])])
 
+#******************************************************************
+#*************** define directory, load each patient's median model
+#***********************************************************************
 
+networks_folder="../../all_solutions_models/"
+similarity_folder="../../files/similarity/"
+load(paste0(networks_folder,patientOneOriginal,".RData"))
+patientOne=apply(PatientResults$AllNwsPatient, 2, median)
+load(paste0(networks_folder,patientTwoOriginal,".RData"))
+patientTwo=apply(PatientResults$AllNwsPatient, 2, median)
 
-# load median models
+#***********************************************************************
+# *************** calculate similarity
+#***********************************************************************
 
-load(paste("../../files/median_models/allMedianModels.RData",sep=""))        # called allMedianNetworks            
+distanceModels=jaccNello(patientOne,patientTwo)
 
-
-
-
-# compute pairwise similarity 
-similarityMatrix = sapply(unname(row.names(allMedianNetworks)), function(x){sapply(unname(row.names(allMedianNetworks)), function(y){return(SMC_of_2networks(allMedianNetworks[x,], allMedianNetworks[y,]))})})  
-diag(similarityMatrix) = NA
-
-# save similarity Matrix
-save(similarityMatrix, file=file.path(files_Similarity_folder,"similarityMatrix.RData"))            
-
-
-
+patientPair=paste0(patientOneOriginal,"_",patientTwoOriginal,".RData")
+cat("----------saving similarity for",patientPair,"\n")
+save(distanceModels,file=paste0(similarity_folder,patientPair))
--- code/similarity/calculateSimilarityBetweenMedianModel.R
+++ code/similarity/calculateSimilarityBetweenMedianModel.R
@@ -1,7 +1,14 @@
+
 #***********************************************************************
 # This scripts loads the results of loadAllModelsPlaneNW2.R,
 # which is the concatenation of all networks solutions within 5% reltol for 
 # the runs that did not crush out of 10 attempts per patient
+# 
+# by Marti Bernardo-Faura.
+# 
+# 
+# 
+# adapted by Melanie Rinas, 2019
 #***********************************************************************
 
 # Use relative paths instead of absolut paths for the files
@@ -9,14 +16,28 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # setwd('~/Documents/combiMS/code/similarity/')
 
-source("./newJaccard.R")
+
+source("../utils/SMC_of_2networks.R")    
+
+
+
+files_Similarity_folder = "../../files/similarity"
+
+
 
 # load median models
-load('../../files/allMedianModels.RData')
+
+load(paste("../../files/median_models/allMedianModels.RData",sep=""))        # called allMedianNetworks            
+
+
+
 
 # compute pairwise similarity 
-similarityMatrix = sapply(unname(row.names(allMedianNetworks)), function(x){sapply(unname(row.names(allMedianNetworks)), function(y){return(jaccNello(allMedianNetworks[x,], allMedianNetworks[y,]))})})
+similarityMatrix = sapply(unname(row.names(allMedianNetworks)), function(x){sapply(unname(row.names(allMedianNetworks)), function(y){return(SMC_of_2networks(allMedianNetworks[x,], allMedianNetworks[y,]))})})  
 diag(similarityMatrix) = NA
 
 # save similarity Matrix
-save(similarityMatrix, file='../../files/similarityMatrix.RData')
+save(similarityMatrix, file=file.path(files_Similarity_folder,"similarityMatrix.RData"))            
+
+
+
