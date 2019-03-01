afaaf223a3e7cca5f897c9163555c41853812828
Mel-Ri <rinas@combine.rwth-aachen.de>
2019-03-01 12:52:31 +0100

    Removed some comments

--- code/utils/SMC_of_2networks.R
+++ code/utils/SMC_of_2networks.R
@@ -25,19 +25,3 @@ SMC_of_2networks <- function(bString_network1, bString_network2) {
    sum(bString_network1 == bString_network2) / length(bString_network1)
    
 }
-
-
-
-
-
-
-# 
-# # Testing SMC_fct:
-# 
-# n1=c(0,0,1,1)
-# n2=c(0,1,0,1)
-# 
-# number_of_matching_attributes__M00_plus_M11 = sum(n1 == n2)
-# number_of_attributes__M00_plus_M01_plus_M10_plus_M11 = length(n1)
-# 
-# SMC_fct(n1,n2)
