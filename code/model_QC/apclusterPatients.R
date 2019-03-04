327ca5d2775dd430a9c967d99951b298bf380b87
Mel-Ri <rinas@combine.rwth-aachen.de>
2019-03-04 14:53:06 +0100

    Adapted for directory structure of the GitHub project
    Minor modifications regarding the figure styling used for the manuscript

--- code/model_QC/apclusterPatients.R
+++ code/model_QC/apclusterPatients.R
@@ -1,7 +1,11 @@
 # Function to discard bias of final patient models due to technical factors such as
 # treatment, center, disease subtipe.
 
-#Final version October 2014, Marti Bernardo-Faura
+# Final version October 2014, Marti Bernardo-Faura
+#
+# Minor modifications regarding figure styling and Github repository directories, 2019, Melanie Rinas
+
+
 
 #apclusterPatients<-function(Models){
   
@@ -12,20 +16,91 @@
   library(NMF)
   library(corrplot)
   
+  # define directories / file locations  ------------------------------------------------------------------------------------------------------------------------------------------------------------
   
-  load("/Users/marti/Documents/R/combiMS/modelling/AvgModelsInTol.RData")
-  Models=AvgModelsInTol
+  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  
+  
+  
+  
+  # ######################################################
+  # get annotation data 
+  # # ######################################################
+  
+  annot=read.csv("../../files/annotations/annot169pat_v2.csv",header=TRUE,dec=".",check.names=TRUE, stringsAsFactors=FALSE)
+  
+  
+  
+  
+  #simplify category
+  annot2=annot
+  annot2$Category[which(annot$Category=="dont know")]='PPMS'
+  annot2$Category[which(annot2$Category=='PPMS slow')]='PPMS'
+  annot2$Category[which(annot2$Category=='PPMS fast')]='PPMS'
+  
+  
+  rm(annot)
+  
+  
+  
+  
+  annot3 = annot2
+  
+  annot3$HealthStatus = annot2$Disease.Subtype
+  #annot3$HealthStatus[which(annot2$Group=="Healthy")]='Healthy controls'
+  annot3$HealthStatus[which(annot2$Group=="Healthy")]='Healthy'
+  
+  annot3$HealthStatus[which(annot2$Disease.Subtype=="PP")]='PPMS'
+  annot3$HealthStatus[which(annot2$Disease.Subtype=="PR")]=annot2$Category[which(annot2$Disease.Subtype=="PR")]
+  annot3$HealthStatus[which(annot2$Disease.Subtype=="RR")]='RRMS'
+  
+  annot3$HealthStatus[which(annot2$Disease.Subtype=="SP")]=annot2$Category[which(annot2$Disease.Subtype=="SP")]
+  #annot3$HealthStatus[which(annot2$Disease.Subtype=="SP")]='SPMS'
+  
+  
+  annot3$Condition = annot2$condition
+  
+  annot3$Treatment[which(annot2$Treatment=="Copaxone")]='GA'
+  annot3$Treatment[which(annot2$Treatment=="Gilenya")]='FTY'
+  annot3$Treatment[which(annot2$Treatment=="Tysabri")]='NTZ'
+  annot3$Treatment[which(annot2$condition=="Untreated")]='Untreated MS'
+  annot3$Treatment[which(annot2$condition=="Untreated")]='Untreated MS'
+  annot3$Treatment[which(annot2$condition=="Healthy")]='Healthy'
+  annot3$Treatment[which(annot2$condition=="Tysabri or Placebo")]='NTZ or Placebo'
+  
+  
+  annot = annot3
+  
+  
+  
+  
+  
+  
+  # *************************************************************************************************************************
+  # ***********load predicted networks
+  # *************************************************************************************************************************
+  
+  load(paste("../../files/median_models/allMedianModels.RData",sep=""))  # called allMedianNetworks               
+  
+  Models=allMedianNetworks
 
+  
+  
+  
+  
+  
   # ************************************ 
-  # calculate patient correaltion as similarity matrix
+  # calculate patient correlation as similarity matrix
   # ************************************
   #Cor=cor(t(Models))
   D=dist(Models)
   Cor=as.matrix(D)
   #aheatmap(Cor)
   #corrplot(Cor,order = "hclust", addrect = 3)
-  annot=read.csv("/Users/marti/Documents/R/combiMS/modelling/annot169pat_v2.csv",header=TRUE,dec=".",check.names=TRUE, stringsAsFactors=FALSE)
-  aheatmap(Cor, annColors=c("Paired","Dark2","Accent"),distfun="euclidean", annRow=annot[,c("Disease.Subtype","condition","Treatment")])
+
+  #aheatmap(Cor, annColors=c("Paired","Dark2","Accent"),distfun="euclidean", annRow=annot[,c("Disease.Subtype","condition","Treatment")])
+  
+  
   
   # ************************************ 
   # ap cluster using similiartiy matrix
@@ -44,13 +119,13 @@
   # ********* asign cluster names to patients
   df$cluster=vector(length=length(df$value)) #add an empty column to the df to contain the clusters
   df$exemplar=vector(length=length(df$value)) #add an empty column to the df to contain the exemplars
-  annot$cluster=vector(length=length(annot$ID))
+  annot$Cluster=vector(length=length(annot$ID))
   
   for (index in 1:length(clustered@clusters)){  
     df$cluster[which(df$patient %in% names(clustered@clusters[[index]])==TRUE)]=index
     df$exemplar[which(df$patient %in% names(clustered@clusters[[index]])==TRUE)]=names(clustered@exemplars[index])    
     # asign cluster to annotation file
-    annot$cluster[which(annot$ID %in% names(clustered@clusters[[index]])==TRUE)]=index
+    annot$Cluster[which(annot$ID %in% names(clustered@clusters[[index]])==TRUE)]=index
     
   }
   
@@ -58,22 +133,59 @@
   # plot heatmap of patients annotated by cluster
   # ************************************ 
   
-  ModelsOrderedClustered=Models[order(annot$cluster),]
-  annotOrdered=annot[order(annot$cluster),]
-  aheatmap(ModelsOrderedClustered, Rowv=NA, Colv=NA,annColors=c("Paired","Dark2","Accent","Dark2","Accent"),distfun="euclidean", annRow=annotOrdered[,c("cluster","Disease.Subtype","condition","Treatment","Center")])
+  ModelsOrderedClustered=Models[order(annot$Cluster),]
+  annotOrdered=annot[order(annot$Cluster),]
+  
+  
+  aheatmap(ModelsOrderedClustered, Rowv=NA, Colv=NA,annColors=c("Paired","Dark2","Accent","Dark2","Accent"),distfun="euclidean", annRow=annotOrdered[,c("Cluster","Disease.Subtype","condition","Treatment","Center")])
+  
+  
+  
+  # Define colores
+  
+  color_vec_clusters = c("#D53E4F","#F46D43","#FDAE61","#FEE08B" ,"#FFFFBF", "#E6F598", "#ABDDA4" ,"#66C2A5", "#5E4FA2")
+  
+  color_annotation = list(color_vec_clusters,                                                   # color for clusters
+                          c('#E69679','#407FB7','#A8859E','#89CCCF'),                                           # color for disease subtype
+                          c('#407FB7', '#8DC060', '#FABE50'),                        # color for condition
+                          c("#0098A1","grey40","#57AB27","#BDCD00","#407FB7","#612158",'lightcyan2',"#7A6FAC",'grey80','#FABE50'),                                                  # color for treatment
+                          c('#2D7F83', '#B65256', '#D0D95C', '#9B91C1'))                                                              # color for center
+  
+  
+  
+  
+  pdf(file.path("../../figures/supplementary_merging/",paste("aheatmap_allMedianNetworks_ModelsCluster_QC.pdf",sep = "")))
+  
+  
+  aheatmap(ModelsOrderedClustered, Rowv=NA, Colv=NA,
+           color = c('grey90','black'),
+           annColors = color_annotation,distfun="euclidean", annRow=annotOrdered[,c("Cluster","HealthStatus","Condition","Treatment","Center")])
+  
+  
+  dev.off()
+  
+  
+  
+  
+  
+  
+  
+  
   
   # ************************************ 
   # subset to test plotting small num of clusters
   # ***********************************
   # subset to plot a single cluster
-  dfCluster16=df[which(df$patient %in% names(clustered@clusters[[16]])),]
+  # 
+  test_cluster_ID = 3
+  dfClusterTest=df[which(df$patient %in% names(clustered@clusters[[test_cluster_ID]])),]
   
-  dfCluster16$cluster=factor(dfCluster16$cluster) # turn the cluster number into a factor so that plotting does not treat it as a continuos var
-  ggplot(dfCluster16)+geom_point(aes(x=interaction,y=value,colour=patient))
-  ggplot(dfCluster16)+geom_boxplot(aes(x=patient,y=value))
+  dfClusterTest$cluster=factor(dfClusterTest$cluster) # turn the cluster number into a factor so that plotting does not treat it as a continuos var
+  ggplot(dfClusterTest)+geom_point(aes(x=interaction,y=value,colour=patient))
+  ggplot(dfClusterTest)+geom_boxplot(aes(x=patient,y=value))
   
   # subset to plot three clusters
-  dfThreeClusters=df[which(df$patient %in% c(names(clustered@clusters[[10]]),names(clustered@clusters[[11]]),names(clustered@clusters[[12]]))),]
+  dfThreeClusters=df[which(df$patient %in% c(names(clustered@clusters[[1]]),names(clustered@clusters[[2]]),names(clustered@clusters[[3]]))),]
   ThreeExemplars=dfThreeClusters[which(dfThreeClusters$patient %in% unique(dfThreeClusters$exemplar)),]
   
   dfThreeClusters$cluster=factor(dfThreeClusters$cluster) # turn the cluster number into a factor so that plotting does not treat it as a continuos var
